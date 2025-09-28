8660a4426640c56719dceba419a839b43114a84e
Prince Vuha <princevuha@yahoo.com>
2025-09-28 16:43:58 +0000

    create a card component and update interfaces

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,11 @@
+import Card from '@/components/common/Card'
 import React from 'react'
 
 const home = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
+            <Card title="Card Title" content="This is the content of the card." />
         </div>
     )
 }
