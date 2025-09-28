8d035b240f0a1eccad2fb89986bab5c2494233bb
Prince Vuha <princevuha@yahoo.com>
2025-09-28 16:33:17 +0000

    add a home and about page

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to this website</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+
+                </ul>
+            </nav>
         </div>
     )
 }
