286e032df0e756808d7f8d4818be8d5306e9397e
Prince Vuha <princevuha@yahoo.com>
2025-09-28 16:38:16 +0000

    include header in the home page

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/layout/Header'
 import React from 'react'
 
 const index = () => {
   return (
     <div>
+      <Header />
       <h1>Hello World</h1>
     </div>
   )
